be8da37eaccc8c6003b5670fa47cfecc37d392f0
Khanya <kilakanya@gmail.com>
2019-01-06 13:37:21 +0200

    Add project files.

---
+++ ASP_MVC5_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_MVC5_Project.Startup))]
+namespace ASP_MVC5_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
