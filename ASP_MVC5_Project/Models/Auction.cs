0c8c541a494f97ec9f9e68fc8d0c4ac31f74a186
Khanya <kilakanya@gmail.com>
2019-01-10 22:21:48 +0200

    Added Views...

---
+++ ASP_MVC5_Project/Models/Auction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MVC5_Project.Models
+{
+    public class Auction
+    {
+        public long id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public decimal StartPrice { get; set; }
+        public decimal? CurrentPrice { get; set; }
+    }
+}
